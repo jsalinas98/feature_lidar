c5ffefd78ef3c6d5c2a6d9eb374562e1ec1dbf52
pedgilgam <pedrogilgam@gmail.com>
2020-05-16 11:46:46 +0200

    Sacar normales, funciona o al menos compila y va pero no muestra en rviz

--- src/roscpp_pcl.cpp
+++ src/roscpp_pcl.cpp
@@ -1,68 +1,106 @@
 /*
-          * PCL Example using ROS and CPP
-          */
+* PCL Example using ROS and CPP
+*/
 
-         // Include the ROS library
-         #include <ros/ros.h>
+// Include the ROS library
+#include <ros/ros.h>
 
-         // Include pcl
-         #include <pcl_conversions/pcl_conversions.h>
-         #include <pcl/point_cloud.h>
-         #include <pcl/point_types.h>
-         #include <pcl/filters/voxel_grid.h>
+// Include pcl
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 
-         // Include PointCloud2 message
-         #include <sensor_msgs/PointCloud2.h>
+// Include PointCloud2 message
+#include <sensor_msgs/PointCloud2.h>
 
-         // Topics
-         static const std::string IMAGE_TOPIC = "/velodyne_points";
-         static const std::string PUBLISH_TOPIC = "/pcl/points";
+/******************************************************************************************************************/
+// Prueba Superficies normales
+#include <pcl/features/normal_3d.h>
+/******************************************************************************************************************/
 
-         // ROS Publisher
-         ros::Publisher pub;
+// Topics
+static const std::string IMAGE_TOPIC = "/velodyne_points";
+static const std::string PUBLISH_TOPIC = "/pcl/points";
 
-         void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
-         {
-             // Container for original & filtered data
-             pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2;
-             pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-             pcl::PCLPointCloud2 cloud_filtered;
+// ROS Publisher
+ros::Publisher pub;
 
-             // Convert to PCL data type
-             pcl_conversions::toPCL(*cloud_msg, *cloud);
+void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+{
+	// Container for original & filtered data
+	pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2;
+	pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+	pcl::PCLPointCloud2 cloud_filtered;
 
-             // Perform the actual filtering
-             pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-             sor.setInputCloud (cloudPtr);
-             sor.setLeafSize (0.1, 0.1, 0.1);
-             sor.filter (cloud_filtered);
+	// Convert to PCL data type
+	pcl_conversions::toPCL(*cloud_msg, *cloud);
 
-             // Convert to ROS data type
-             sensor_msgs::PointCloud2 output;
-             pcl_conversions::moveFromPCL(cloud_filtered, output);
+	// Perform the actual filtering
+	pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+	sor.setInputCloud (cloudPtr);
+	sor.setLeafSize (0.1, 0.1, 0.1);
+	sor.filter (cloud_filtered);
 
-             // Publish the data
-             pub.publish (output);
-         }
 
-         int main (int argc, char** argv)
-         {
-             // Initialize the ROS Node "roscpp_pcl_example"
-             ros::init (argc, argv, "roscpp_pcl_example");
-             ros::NodeHandle nh;
 
-             // Print "Hello" message with node name to the terminal and ROS log file
-             ROS_INFO_STREAM("Hello from ROS Node: " << ros::this_node::getName());
+	/******************************************************************************************************************/
+	//Prueba Superficies Normales
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_nueva (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::fromPCLPointCloud2 (cloud_filtered, *cloud_nueva);
 
-             // Create a ROS Subscriber to IMAGE_TOPIC with a queue_size of 1 and a callback function to cloud_cb
-             ros::Subscriber sub = nh.subscribe(IMAGE_TOPIC, 1, cloud_cb);
+	// Create the normal estimation class, and pass the input dataset to it
+	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+	ne.setInputCloud (cloud_nueva);
 
-             // Create a ROS publisher to PUBLISH_TOPIC with a queue_size of 1
-             pub = nh.advertise<sensor_msgs::PointCloud2>(PUBLISH_TOPIC, 1);
+	// Create an empty kdtree representation, and pass it to the normal estimation object.
+	// Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+	ne.setSearchMethod (tree);
 
-             // Spin
-             ros::spin();
+	// Output datasets
+	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
 
-             // Success
-             return 0;
-         }
+	// Use all neighbors in a sphere of radius 3cm
+	ne.setRadiusSearch (0.03);
+
+	// Compute the features
+	ne.compute (*cloud_normals);
+	/******************************************************************************************************************/
+
+
+
+	// Convert to ROS data type
+	sensor_msgs::PointCloud2 output;
+	//pcl_conversions::fromPCL(cloud_filtered, output);   //Conversion antigua
+
+	/******************************************************************************************************************/
+	pcl::toROSMsg(*cloud_normals, output);    //Conversion nueva
+	//No da error e imprime el mensaje, pero no lo visualizo en el rviz
+	/******************************************************************************************************************/
+
+	// Publish the data
+	pub.publish (output);
+}
+
+int main (int argc, char** argv)
+{
+	// Initialize the ROS Node "roscpp_pcl_example"
+	ros::init (argc, argv, "roscpp_pcl_example");
+	ros::NodeHandle nh;
+
+	// Print "Hello" message with node name to the terminal and ROS log file
+	ROS_INFO_STREAM("Hello from ROS Node: " << ros::this_node::getName());
+
+	// Create a ROS Subscriber to IMAGE_TOPIC with a queue_size of 1 and a callback function to cloud_cb
+	ros::Subscriber sub = nh.subscribe(IMAGE_TOPIC, 1, cloud_cb);
+
+	// Create a ROS publisher to PUBLISH_TOPIC with a queue_size of 1
+	pub = nh.advertise<sensor_msgs::PointCloud2>(PUBLISH_TOPIC, 1);
+
+	// Spin
+	ros::spin();
+
+	// Success
+	return 0;
+}
