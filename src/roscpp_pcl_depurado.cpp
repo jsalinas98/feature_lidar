9a0cc4ce48a21fa8e9f30834171564937074ee8b
jsalinas98 <jsalinashdez@gmail.com>
2020-05-22 18:56:18 +0200

    depurado con instantes anteriores

--- src/roscpp_pcl_depurado.cpp
+++ src/roscpp_pcl_depurado.cpp
@@ -37,6 +37,13 @@ static const std::string IMAGE_TOPIC = "/velodyne_points";
 static const std::string PUBLISH_TOPIC = "/pcl/points";
 static const std::string PUBLISH_TOPIC2 = "/pcl/points2";
 
+//Variables globales
+pcl::PointCloud<pcl::PointXYZI>::Ptr keypoints_anterior (new pcl::PointCloud<pcl::PointXYZI>);
+pcl::PointCloud<pcl::PFHSignature125>::Ptr descriptorPFH_anterior(new pcl::PointCloud<pcl::PFHSignature125>());
+pcl::PointCloud<pcl::FPFHSignature33>::Ptr descriptorFPHF_anterior(new pcl::PointCloud<pcl::FPFHSignature33>());
+pcl::PointCloud<pcl::VFHSignature308>::Ptr descriptorVFH_anterior(new pcl::PointCloud<pcl::VFHSignature308>);
+
+
 // ROS Publisher
 ros::Publisher pubF;
 ros::Publisher pubKP;
@@ -44,15 +51,15 @@ ros::Publisher pubKP;
 // ROS Subscriber
 ros::Subscriber sub;
 
-pcl::PointCloud<pcl::Normal>::Ptr Normals(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
+pcl::PointCloud<pcl::Normal>::Ptr Normals(const pcl::PointCloud<pcl::PointXYZI>::Ptr cloud)
 {
 	// Create the normal estimation class, and pass the input dataset to it
-	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+	pcl::NormalEstimation<pcl::PointXYZI, pcl::Normal> ne;
 	ne.setInputCloud (cloud);
 
 	// Create an empty kdtree representation, and pass it to the normal estimation object.
 	// Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
-	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+	pcl::search::KdTree<pcl::PointXYZI>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZI> ());
 	ne.setSearchMethod (tree);
 
 	// Output datasets
@@ -67,16 +74,16 @@ pcl::PointCloud<pcl::Normal>::Ptr Normals(const pcl::PointCloud<pcl::PointXYZ>::
 	return cloud_normals;
 }
 
-pcl::PointCloud<pcl::PFHSignature125>::Ptr PFH(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, const pcl::PointCloud<pcl::Normal>::Ptr cloud_normals)
+pcl::PointCloud<pcl::PFHSignature125>::Ptr PFH(const pcl::PointCloud<pcl::PointXYZI>::Ptr cloud, const pcl::PointCloud<pcl::Normal>::Ptr cloud_normals)
 {
 	// PFH estimation object.
-	pcl::PFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::PFHSignature125> pfh;
+	pcl::PFHEstimation<pcl::PointXYZI, pcl::Normal, pcl::PFHSignature125> pfh;
 	pfh.setInputCloud (cloud);
 	pfh.setInputNormals (cloud_normals);
 	
 	// Create an empty kdtree representation, and pass it to the normal estimation object.
 	// Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
-	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+	pcl::search::KdTree<pcl::PointXYZI>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZI> ());
 	pfh.setSearchMethod (tree);
 
 	// Object for storing the PFH descriptors for each point.
@@ -100,16 +107,16 @@ pcl::PointCloud<pcl::PFHSignature125>::Ptr PFH(const pcl::PointCloud<pcl::PointX
 }
 
 
-pcl::PointCloud<pcl::FPFHSignature33>::Ptr FPFH(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, const pcl::PointCloud<pcl::Normal>::Ptr cloud_normals)
+pcl::PointCloud<pcl::FPFHSignature33>::Ptr FPFH(const pcl::PointCloud<pcl::PointXYZI>::Ptr cloud, const pcl::PointCloud<pcl::Normal>::Ptr cloud_normals)
 {
 	// FPFH estimation object.
-	pcl::FPFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::FPFHSignature33> fpfh;
+	pcl::FPFHEstimation<pcl::PointXYZI, pcl::Normal, pcl::FPFHSignature33> fpfh;
 	fpfh.setInputCloud (cloud);
 	fpfh.setInputNormals (cloud_normals);
 	
 	// Create an empty kdtree representation, and pass it to the normal estimation object.
 	// Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
-	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+	pcl::search::KdTree<pcl::PointXYZI>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZI> ());
 	fpfh.setSearchMethod (tree);
 
 	// Object for storing the FPFH descriptors for each point.
@@ -133,15 +140,15 @@ pcl::PointCloud<pcl::FPFHSignature33>::Ptr FPFH(const pcl::PointCloud<pcl::Point
 	return descriptor;
 }
 
-pcl::PointCloud<pcl::VFHSignature308>::Ptr VFH(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, const pcl::PointCloud<pcl::Normal>::Ptr cloud_normals)
+pcl::PointCloud<pcl::VFHSignature308>::Ptr VFH(const pcl::PointCloud<pcl::PointXYZI>::Ptr cloud, const pcl::PointCloud<pcl::Normal>::Ptr cloud_normals)
 {
 	// Object for storing the VFH descriptor.
 	pcl::PointCloud<pcl::VFHSignature308>::Ptr descriptor(new pcl::PointCloud<pcl::VFHSignature308>);
 
-	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+	pcl::search::KdTree<pcl::PointXYZI>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZI>);
 
 	// VFH estimation object.
-	pcl::VFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::VFHSignature308> vfh;
+	pcl::VFHEstimation<pcl::PointXYZI, pcl::Normal, pcl::VFHSignature308> vfh;
 	vfh.setInputCloud(cloud);
 	vfh.setInputNormals(cloud_normals);
 	vfh.setSearchMethod(tree);
@@ -164,15 +171,15 @@ pcl::PointCloud<pcl::VFHSignature308>::Ptr VFH(const pcl::PointCloud<pcl::PointX
 	return descriptor;
 }
 
-pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsHarris(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
+pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsHarris(const pcl::PointCloud<pcl::PointXYZI>::Ptr cloud)
 {
 	pcl::PointCloud<pcl::PointXYZI>::Ptr keypoints (new pcl::PointCloud<pcl::PointXYZI>);
 
-	pcl::HarrisKeypoint3D <pcl::PointXYZ, pcl::PointXYZI> detector;
+	pcl::HarrisKeypoint3D <pcl::PointXYZI, pcl::PointXYZI> detector;
 	detector.setNonMaxSupression (true);
 	detector.setInputCloud (cloud);
 	detector.setThreshold (1e-11);
-	detector.setMethod(pcl::HarrisKeypoint3D<pcl::PointXYZ,pcl::PointXYZI>::HARRIS); 
+	detector.setMethod(pcl::HarrisKeypoint3D<pcl::PointXYZI,pcl::PointXYZI>::HARRIS); 
 	detector.setRefine(false);
 	detector.setRadius(0.5); 
 	detector.compute (*keypoints);
@@ -182,7 +189,7 @@ pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsHarris(const pcl::PointCloud<pcl::
 	return keypoints;
 }
 
-pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsSiftNE(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
+pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsSiftNE(const pcl::PointCloud<pcl::PointXYZI>::Ptr cloud)
 {
 	// Parameters for sift computation
 	const float min_scale = 0.01f;
@@ -191,9 +198,9 @@ pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsSiftNE(const pcl::PointCloud<pcl::
 	const float min_contrast = 0.001f;
 
 	// Estimate the normals of the cloud_xyz
-	pcl::NormalEstimation<pcl::PointXYZ, pcl::PointNormal> ne;
+	pcl::NormalEstimation<pcl::PointXYZI, pcl::PointNormal> ne;
 	pcl::PointCloud<pcl::PointNormal>::Ptr cloud_normals (new pcl::PointCloud<pcl::PointNormal>);
-	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>());
+	pcl::search::KdTree<pcl::PointXYZI>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZI>());
 
 	ne.setInputCloud(cloud);
 	ne.setSearchMethod(tree);
@@ -228,17 +235,17 @@ pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsSiftNE(const pcl::PointCloud<pcl::
 namespace pcl
 {
   template<>
-    struct SIFTKeypointFieldSelector<PointXYZ>
+    struct SIFTKeypointFieldSelector<PointXYZI>
     {
       inline float
-      operator () (const PointXYZ &p) const
+      operator () (const PointXYZI &p) const
       {
 	return p.z;
       }
     };
 }
 
-pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsSiftZ(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
+pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsSiftZ(const pcl::PointCloud<pcl::PointXYZI>::Ptr cloud)
 {  
 	// Parameters for sift computation
 	const float min_scale = 0.005f;
@@ -247,9 +254,9 @@ pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsSiftZ(const pcl::PointCloud<pcl::P
 	const float min_contrast = 0.005f;
 
 	// Estimate the sift interest points using z values from xyz as the Intensity variants
-	pcl::SIFTKeypoint<pcl::PointXYZ, pcl::PointXYZI> sift;
+	pcl::SIFTKeypoint<pcl::PointXYZI, pcl::PointXYZI> sift;
 	pcl::PointCloud<pcl::PointXYZI>::Ptr result(new pcl::PointCloud<pcl::PointXYZI>);
-	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ> ());
+	pcl::search::KdTree<pcl::PointXYZI>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZI> ());
 	sift.setSearchMethod(tree);
 	sift.setScales(min_scale, n_octaves, n_scales_per_octave);
 	sift.setMinimumContrast(min_contrast);
@@ -261,12 +268,12 @@ pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsSiftZ(const pcl::PointCloud<pcl::P
 	return result;
 }
 
-pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsISS(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
+pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsISS(const pcl::PointCloud<pcl::PointXYZI>::Ptr cloud)
 {
 
-	pcl::ISSKeypoint3D<pcl::PointXYZ, pcl::PointXYZI> iss_detector;
+	pcl::ISSKeypoint3D<pcl::PointXYZI, pcl::PointXYZI> iss_detector;
 	pcl::PointCloud<pcl::PointXYZI>::Ptr result (new pcl::PointCloud<pcl::PointXYZI>());
-	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>());
+	pcl::search::KdTree<pcl::PointXYZI>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZI>());
 
 	iss_detector.setSearchMethod(tree);
 	iss_detector.setSalientRadius(10 * 0.05);
@@ -286,28 +293,28 @@ pcl::PointCloud<pcl::PointXYZI>::Ptr KeyPointsISS(const pcl::PointCloud<pcl::Poi
 void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
 	// Container for original data
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
 	pcl::fromROSMsg(*cloud_msg, *cloud);
 
 	// VoxedGrid Filter
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZI>);
 
-	pcl::VoxelGrid<pcl::PointXYZ> sor;
+	pcl::VoxelGrid<pcl::PointXYZI> sor;
 	sor.setInputCloud (cloud);
 	sor.setLeafSize (0.1, 0.1, 0.1);
 	sor.filter (*cloud_filtered);
 
 	// PassThrough Filter in X
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered2(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered2(new pcl::PointCloud<pcl::PointXYZI>);
 
-	pcl::PassThrough<pcl::PointXYZ> pass;
+	pcl::PassThrough<pcl::PointXYZI> pass;
 	pass.setInputCloud(cloud_filtered);
 	pass.setFilterFieldName("x");
 	pass.setFilterLimits(-10, 10);
 	pass.filter(*cloud_filtered2);
 
 	// PassThrough Filter in Y
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered3(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered3(new pcl::PointCloud<pcl::PointXYZI>);
 
 	pass.setInputCloud(cloud_filtered2);
 	pass.setFilterFieldName("y");
@@ -324,6 +331,28 @@ void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 	cloud_KPSiftNE=KeyPointsSiftNE(cloud_filtered3);
 	cloud_KPSiftZ=KeyPointsSiftZ(cloud_filtered3);
 	cloud_KPISS=KeyPointsISS(cloud_filtered3);
+
+	pcl::PointCloud<pcl::Normal>::Ptr cloud_normal(new pcl::PointCloud<pcl::Normal>);
+
+	cloud_normal = Normals(cloud_filtered3);
+
+	pcl::PointCloud<pcl::PFHSignature125>::Ptr descriptorPFH_actual(new pcl::PointCloud<pcl::PFHSignature125>());
+	pcl::PointCloud<pcl::FPFHSignature33>::Ptr descriptorFPFH_actual(new pcl::PointCloud<pcl::FPFHSignature33>());
+	pcl::PointCloud<pcl::VFHSignature308>::Ptr descriptorVFH_actual(new pcl::PointCloud<pcl::VFHSignature308>());
+
+	descriptorPFH_actual = PFH(cloud_filtered3, cloud_normal);
+	descriptorFPFH_actual = FPFH(cloud_filtered3, cloud_normal);
+	descriptorVFH_actual = VFH(cloud_filtered3, cloud_normal);
+
+	if (keypoints_anterior != NULL){
+		std::cout << "Puntos nube anterior: " << keypoints_anterior->points.size() << "  -  Puntos nube actual: " << cloud_KPSiftZ->points.size() << std::endl;
+	}
+
+	keypoints_anterior = cloud_KPSiftZ;
+
+	descriptorPFH_anterior = descriptorPFH_actual;
+	descriptorFPHF_anterior = descriptorFPFH_actual;
+	descriptorVFH_anterior = descriptorVFH_actual;
 
 	// Preparo como mensajes la nube filtrada y los KeyPoints que quiero visualizar
    	sensor_msgs::PointCloud2 outputF;
