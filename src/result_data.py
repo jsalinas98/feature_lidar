3d733fae8cadaf3eee120a1b3af3d004a6127fcd
jsalinas98 <jsalinashdez@gmail.com>
2020-05-24 15:33:39 +0200

    nodo graficas actualizado

--- src/result_data.py
+++ src/result_data.py
@@ -4,20 +4,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from std_msgs.msg import Float32
+from std_msgs.msg import Int32
 
 valores = np.array([])
 
 def callback(data):
+	global valores
+
 	rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
-	np.append(valores, data.data)
-	if(valores.size>20):
-		plt.plot(np.arange(0,valores.size,1),valores)
+	valores = np.append(valores, data.data)
+
+def callback_print(data):
+	rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+	if data.data != 0:
+		print(valores)
+		print(valores.size)
+		plt.plot(range(valores.size),valores)
+		plt.axis([0, valores.size, 0, 15])
 		plt.show()
 
 def listener():
 
 	rospy.init_node('listener', anonymous=True)
 	rospy.Subscriber('/data/ToPy', Float32, callback)
+	rospy.Subscriber('/data/PrintGraph', Int32, callback_print)
 
 	rospy.spin()
 
